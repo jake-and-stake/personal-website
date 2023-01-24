92835e34bdba21fa6f0719d535d40825ade56c77
jake-and-stake <0xvanquisher@gmail.com>
2023-01-24 14:15:41 -0800

    Add database connection to MongoDB Atlas product.

--- microblog/app.py
+++ microblog/app.py
@@ -1,14 +1,29 @@
 import datetime
 from flask import Flask, render_template, request
+from pymongo import MongoClient
+import certifi
 
 app = Flask(__name__)
+client = MongoClient("mongodb+srv://jake:<password>@migroblogapp.gkdptsi.mongodb.net/test",
+                    tlsCAFile=certifi.where())
+app.db = client.microblog
+entries = []
 
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
         entry_content = request.form.get("content")
         formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
-        print(entry_content, formatted_date)
-    return render_template("home.html")
+        entries.append((entry_content, formatted_date))
+        app.db.entries.insert_one({"content": entry_content, "date": formatted_date})
 
+    entries_with_date = [
+        (
+            entry[0],
+            entry[1],
+            datetime.datetime.strptime(entry[1], "%Y-%m-%d").strftime("%b %d")
+        )
+        for entry in entries
+    ]
+    return render_template("home.html", entries=entries_with_date)
 
