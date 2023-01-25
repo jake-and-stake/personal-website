c5ff27a07f4f7436cee887fc567aa92d4fb8c1b8
jake-and-stake <0xvanquisher@gmail.com>
2023-01-25 11:29:33 -0800

    Add requirements.txt file

--- microblog/app.py
+++ microblog/app.py
@@ -1,41 +1,37 @@
+import certifi
 import datetime
+import os
+
 from flask import Flask, render_template, request
 from pymongo import MongoClient
-import certifi
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
 MONGO_CLIENT_URL = os.getenv('MONGO_CLIENT_URL')
 
-app = Flask(__name__)
-client = MongoClient(MONGO_CLIENT_URL,
-                    tlsCAFile=certifi.where())
-app.db = client.microblog
-# entries = []
+def create_app():
+    app = Flask(__name__)
+    client = MongoClient(MONGO_CLIENT_URL,
+                        tlsCAFile=certifi.where())
+    app.db = client.microblog
 
-FETCH_RECENT_ENTRIES = {
-    "find": "entries",
-    "sort": "date",
-    "limit": 10
-}
+    @app.route("/", methods=["GET", "POST"])
+    def home():
+        if request.method == "POST":
+            entry_content = request.form.get("content")
+            formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
+            app.db.entries.insert_one({"content": entry_content, "date": formatted_date})
 
-@app.route("/", methods=["GET", "POST"])
-def home():
-    if request.method == "POST":
-        entry_content = request.form.get("content")
-        formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
-        app.db.entries.insert_one({"content": entry_content, "date": formatted_date})
+        entries = app.db.entries.find( {} )
+        entries_with_date = [
+            (
+                entry["content"],
+                entry["date"],
+                datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")
+            )
+            for entry in entries
+        ]
 
-    entries = app.db.entries.find( {} )
-    entries_with_date = [
-        (
-            entry["content"],
-            entry["date"],
-            datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")
-        )
-        for entry in entries
-    ]
-
-    return render_template("home.html", entries=entries_with_date)
-
+        return render_template("home.html", entries=entries_with_date)
+    
+    return app
