ce1a9aed4e66f23e4b312cb3769c53f00cad8648
jake-and-stake <0xvanquisher@gmail.com>
2023-01-25 10:53:21 -0800

    Grab recent entries from MongoDB database.

--- microblog/app.py
+++ microblog/app.py
@@ -12,23 +12,30 @@ app = Flask(__name__)
 client = MongoClient(MONGO_CLIENT_URL,
                     tlsCAFile=certifi.where())
 app.db = client.microblog
-entries = []
+# entries = []
+
+FETCH_RECENT_ENTRIES = {
+    "find": "entries",
+    "sort": "date",
+    "limit": 10
+}
 
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
         entry_content = request.form.get("content")
         formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
-        entries.append((entry_content, formatted_date))
         app.db.entries.insert_one({"content": entry_content, "date": formatted_date})
 
+    entries = app.db.entries.find( {} )
     entries_with_date = [
         (
-            entry[0],
-            entry[1],
-            datetime.datetime.strptime(entry[1], "%Y-%m-%d").strftime("%b %d")
+            entry["content"],
+            entry["date"],
+            datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")
         )
         for entry in entries
     ]
+
     return render_template("home.html", entries=entries_with_date)
 
